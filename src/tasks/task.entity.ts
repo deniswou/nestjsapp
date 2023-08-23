ccc320fe8c9eaf74d538f746d68dab03de574435
deniswou <deniswouh@gmail.com>
2023-08-23 17:13:37 -0400

    module + entity

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,12 @@
+enum Task{
+    PENDING = 'PENDING',
+    IN_PROGRES = 'IN_PROGRESS',
+    DONE = 'DONE',
+}
+
+class Task{
+    id: string
+    title: string
+    description: string
+    status: string
+}
