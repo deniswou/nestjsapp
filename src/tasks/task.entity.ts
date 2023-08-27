a3df0185d7ce68810d3931a57b8436cc322bef73
deniswou <deniswouh@gmail.com>
2023-08-27 18:32:40 -0400

    crear y listar

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,12 +1,12 @@
-enum Task{
-    PENDING = 'PENDING',
-    IN_PROGRES = 'IN_PROGRESS',
-    DONE = 'DONE',
+export enum TaskStatus {
+  PENDING = 'PENDING',
+  IN_PROGRES = 'IN_PROGRESS',
+  DONE = 'DONE',
 }
 
-class Task{
-    id: string
-    title: string
-    description: string
-    status: string
+export class Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/task.dto';
 @Controller('tasks')
 export class TasksController {
-    
+  constructor(private tasksService: TasksService) {}
+
   @Get()
   helloword() {
-    return 'nestjs';
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.tasksService.createTask(newTask.title, newTask.description);
   }
 }
