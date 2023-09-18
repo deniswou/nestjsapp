a5cc1e32d95e2a95e1eeaf2c62a01030315a304d
deniswou <deniswouh@gmail.com>
2023-09-18 12:41:43 -0400

    validaciones finales

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,27 @@
 import { TaskStatus } from '../task.entity';
+import { IsNotEmpty, IsString, MinLength, IsOptional, IsIn } from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+
+  @IsString()
   description: string;
 }
 
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.DONE, TaskStatus.IN_PROGRES])
   status?: TaskStatus;
 }
